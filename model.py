e2149dcd3389838e25c4167d2b5e125613dc115d
Melanie Plageman <melanieplageman@gmail.com>
2022-02-14 12:32:52 -0500

    Refactor
    There were some errors and bugs related to moving IOs from bucket to
    bucket. To solve these, move to using more immutable objects and update
    what methods are inherited.
    
    Using frozensets and sets is one part of this. Also, not reusing the
    pipeline for multiple runs.
    
    The Measurer class was removed and its functionality collapsed. Also
    some comments were added.
    
    Though the chart jupyter notebook was kept, run.py is now the best way
    to run the code and make sure it is up-to-date.

--- model.py
+++ model.py
@@ -1,8 +1,25 @@
-from bucket import GateBucket, DialBucket
+from bucket import Pipeline, GateBucket, DialBucket, IntakeBucket, StopBucket
+
+
+class TestPipeline(Pipeline):
+    def __init__(self):
+        self.intake = IntakeBucket("intake")
+        self.submitted_bucket = SubmittedDialBucket("submitted")
+        self.inflight_bucket = InflightDialBucket("inflight")
+        self.completed_bucket = CompletedGateBucket("completed")
+        self.consumed_bucket = StopBucket("consumed")
+
+        self.prefetched_bucket = PrefetchedGateBucket(
+            "prefetched", self.completed_bucket, self.inflight_bucket)
+
+        super().__init__(self.intake, self.prefetched_bucket,
+                         self.submitted_bucket, self.inflight_bucket,
+                         self.completed_bucket, self.consumed_bucket)
+
 
 class PrefetchedGateBucket(GateBucket):
-    def __init__(self, completed_bucket, inflight_bucket):
-        super().__init__()
+    def __init__(self, name, completed_bucket, inflight_bucket):
+        super().__init__(name)
         self.completed_bucket = completed_bucket
         self.inflight_bucket = inflight_bucket
 
@@ -16,12 +33,12 @@ class PrefetchedGateBucket(GateBucket):
 
         self.desired_move_size_override = None
 
-    def desired_move_size(self, tick):
+    def wanted_move_size(self):
         if self.desired_move_size_override is not None:
-            return self.desired_move_size_override(self, tick)
+            return self.desired_move_size_override(self)
 
-        inflight = self.inflight_bucket.num_ios
-        completed = self.completed_bucket.num_ios
+        inflight = len(self.inflight_bucket)
+        completed = len(self.completed_bucket)
 
         if inflight >= self.max_inflight:
             return 0
@@ -31,17 +48,16 @@ class PrefetchedGateBucket(GateBucket):
 
         return self.min_dispatch
 
-    def run(self, tick):
-        super().run(tick)
-        self.measurer['completion_target_distance'] = self.completion_target_distance
-        self.measurer['max_inflight'] = self.max_inflight
-        self.measurer['min_dispatch'] = self.min_dispatch
+    def run(self):
+        super().run()
+        self.tick_data['completion_target_distance'] = self.completion_target_distance
+        self.tick_data['max_inflight'] = self.max_inflight
+        self.tick_data['min_dispatch'] = self.min_dispatch
+
 
 class SubmittedDialBucket(DialBucket):
-    SUBMISSION_OVERHEAD = 0.1
+    LATENCY = 0.1
 
-    def latency(self):
-        return self.SUBMISSION_OVERHEAD
 
 class InflightDialBucket(DialBucket):
     MAX_IOPS = 10000
@@ -50,25 +66,18 @@ class InflightDialBucket(DialBucket):
     def latency(self):
         # TODO: make this formula better
         completion_latency = self.BASE_COMPLETION_LATENCY
-        if self.num_ios + 1 >= self.MAX_IOPS:
+        if len(self) + 1 >= self.MAX_IOPS:
             completion_latency = 10
-        completion_latency += (0.01 * self.num_ios)
+        completion_latency += (0.01 * len(self))
         return self.BASE_COMPLETION_LATENCY
+
 
 class CompletedGateBucket(GateBucket):
     CONSUMPTION_RATE = 1
     CONSUMPTION_SIZE = 1
 
-    last_consumption = 0
-
-    def desired_move_size(self, tick):
-        try_consume = tick >= self.last_consumption + self.CONSUMPTION_RATE
-        if try_consume:
-            self.last_consumption = tick
+    def wanted_move_size(self):
+        if not self.tick % self.CONSUMPTION_RATE:
             return self.CONSUMPTION_SIZE
 
-        return 0
-
-class ConsumedGateBucket(GateBucket):
-    def desired_move_size(self, tick):
         return 0
--- pipeline_configurer.py
+++ pipeline_configurer.py
@@ -1,40 +1,47 @@
-class PipelineConfigurer:
-    def __init__(self, pipeline):
-        self.pipeline = pipeline
+from dataclasses import dataclass
+from typing import Callable
+from model import TestPipeline
 
-    def environment(self, inflight_cap, submission_overhead, max_iops,
-                    base_completion_latency, consumption_rate,
-                    consumption_size):
 
-        self.pipeline.prefetched_bucket.inflight_cap = inflight_cap
+@dataclass(frozen=True)
+class PipelineConfiguration:
+    inflight_cap: int
+    submission_overhead: int
+    max_iops: int
+    base_completion_latency: int
+    consumption_rate: int
+    consumption_size: int
+    prefetch_distance_algorithm: Callable
+    completed_cap: int
+    completion_target_distance: int
+    min_dispatch: int
+    max_inflight: int
 
-        self.pipeline.submitted_bucket.SUBMISSION_OVERHEAD = submission_overhead
+    def generate_pipeline(self, *args, **kwargs):
+        pipeline = TestPipeline(*args, **kwargs)
 
-        self.pipeline.inflight_bucket.MAX_IOPS = max_iops
-        self.pipeline.inflight_bucket.BASE_COMPLETION_LATENCY = base_completion_latency
+        pipeline.prefetched_bucket.inflight_cap = self.inflight_cap
+        pipeline.submitted_bucket.LATENCY = self.submission_overhead
+        pipeline.inflight_bucket.MAX_IOPS = self.max_iops
+        pipeline.inflight_bucket.base_completion_latency = self.base_completion_latency
+        pipeline.completed_bucket.CONSUMPTION_RATE = self.consumption_rate
+        pipeline.completed_bucket.CONSUMPTION_SIZE = self.consumption_size
 
-        self.pipeline.completed_bucket.CONSUMPTION_RATE = consumption_rate
-        self.pipeline.completed_bucket.CONSUMPTION_SIZE = consumption_size
+        pipeline.prefetched_bucket.desired_move_size_override = self.prefetch_distance_algorithm
+        pipeline.prefetched_bucket.completed_cap = self.completed_cap
 
-    def prefetcher(self, prefetch_distance_algorithm, completed_cap,
-                   completion_target_distance, min_dispatch, max_inflight):
-
-        pbucket = self.pipeline.prefetched_bucket
-
-        pbucket.desired_move_size_override = prefetch_distance_algorithm
-        pbucket.completed_cap = completed_cap
-
-        if completion_target_distance > pbucket.completed_cap:
-            print(f'Cannot set completion_target_distance to {completion_target_distance}. Setting instead to completed_cap {pbucket.completed_cap}.')
-            pbucket.completion_target_distance = pbucket.completed_cap
+        if self.completion_target_distance > self.completed_cap:
+            print(f'Cannot set completion_target_distance to {self.completion_target_distance}. Setting instead to completed_cap {self.completed_cap}.')
+            pipeline.prefetched_bucket.completion_target_distance = self.completed_cap
         else:
-            pbucket.completion_target_distance = completion_target_distance
+            pipeline.prefetched_bucket.completion_target_distance = self.completion_target_distance
 
-        # TODO: add some boundaries for min_dispatch
-        pbucket.min_dispatch = min_dispatch
+        pipeline.prefetched_bucket.min_dispatch = self.min_dispatch
 
-        if max_inflight > pbucket.inflight_cap:
-            print(f'Cannot set max_inflight to {max_inflight}. Setting instead to inflight_cap {inflight_cap}.')
-            pbucket.max_inflight = pbucket.inflight_cap
+        if self.max_inflight > self.inflight_cap:
+            print(f'Cannot set max_inflight to {self.max_inflight}. Setting instead to inflight_cap {self.inflight_cap}.')
+            pipeline.prefetched_bucket.max_inflight = self.inflight_cap
         else:
-            pbucket.max_inflight = max_inflight
+            pipeline.prefetched_bucket.max_inflight = self.max_inflight
+
+        return pipeline
