c724dea3a4092b8a2f05ea65c7a103f5d9951c83
Kaiting Chen <ktchen14@gmail.com>
2022-04-03 12:49:21 -0400

    Move bucket types to core.bucket_type

--- prefetch_modeler/core/__init__.py
+++ prefetch_modeler/core/__init__.py
@@ -3,7 +3,8 @@ __all__ = ('IO', 'Tracer', 'Pipeline', 'Bucket', 'GateBucket', 'DialBucket',
            'TargetCapacityBucket', 'GlobalCapacityBucket')
 
 from prefetch_modeler.core.io import IO, Tracer
-from prefetch_modeler.core.bucket import Pipeline, Bucket, GateBucket, \
-           DialBucket, StopBucket, RateBucket, ThresholdBucket, \
-           CapacityBucket, TargetCapacityBucket, GlobalCapacityBucket
+from prefetch_modeler.core.bucket import Pipeline, Bucket
+from prefetch_modeler.core.bucket_type import GateBucket, DialBucket, \
+    StopBucket, RateBucket, ThresholdBucket, CapacityBucket, \
+    TargetCapacityBucket, GlobalCapacityBucket
 from prefetch_modeler.core.units import Duration, Rate
