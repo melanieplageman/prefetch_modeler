f8bfff4d6df67149a02b8e65818328a48c0ad100
Melanie Plageman <melanieplageman@gmail.com>
2022-05-14 13:15:29 -0400

    test prefetcher

--- prefetch_modeler/test_prefetcher.py
+++ prefetch_modeler/test_prefetcher.py
@@ -1,6 +1,8 @@
 from prefetch_modeler.core import RateBucket, Rate, Interval, Duration
 from dataclasses import dataclass
 from fractions import Fraction
+import itertools
+
 
 @dataclass(kw_only=True)
 class PIDPrefetchInterval(Interval):
@@ -10,28 +12,37 @@ class PIDPrefetchInterval(Interval):
     lt_demanded: int
     lt_completed: int
 
-class TestPrefetcher(RateBucket):
+@dataclass
+class RateLogItem:
+    tick: int
+    raw_rate: Fraction
+    demand_rate: Fraction
+
+class ControlPrefetcher(RateBucket):
     name = 'remaining'
     og_rate = Rate(per_second=6000)
     sample_period = 1000
-    lookback = 66
+    raw_lookback = 66
+    avg_lookback = 10
 
     def __init__(self, *args, **kwargs):
         self.ledger = [PIDPrefetchInterval(tick=0, rate=self.og_rate.value,
                                            completed=0, awaiting_dispatch=0,
                                            inflight=0, lt_demanded=0,
                                            lt_completed=0)]
+
+        self.rate_log = [RateLogItem(tick=0, raw_rate=0, demand_rate=0)]
         self.next_sample = self.sample_period
         super().__init__(*args, **kwargs)
         self._rate = self.rate()
         print(f"Initial Rate: {self._rate} {float(self._rate)}")
 
     def get_periods(self):
-        if len(self.ledger) < self.lookback:
+        if len(self.ledger) < self.raw_lookback:
             return None, None
 
         period_newer = self.ledger[-1]
-        period_older = self.ledger[-1 * self.lookback]
+        period_older = self.ledger[-1 * self.raw_lookback]
 
         # print(f'length: {period_newer.tick - period_older.tick}')
         return period_older, period_newer
@@ -90,13 +101,48 @@ class TestPrefetcher(RateBucket):
 
     @property
     def demand_rate(self):
-        period_older, period_newer = self.get_periods()
-        if period_older is None or period_newer is None:
-            return 0
+        move_record = list(reversed(self.pipeline['completed'].move_record))
+        intervals = list(zip(move_record, move_record[1:]))
 
-        length = period_newer.tick - period_older.tick
-        return Fraction(period_newer.lt_demanded - period_older.lt_demanded,
-                        length)
+        number_moved = 0
+        time_elapsed = 0
+        number_seen = 0
+        for newer_movement, older_movement in intervals:
+            number_moved += newer_movement.number
+            # print(f'newer movement tick: {newer_movement.tick}. older movement tick: {older_movement.tick}')
+            time_elapsed += newer_movement.tick - older_movement.tick
+            number_seen += 1
+            if number_seen > self.raw_lookback:
+                break
+
+
+        if time_elapsed == 0:
+            raw_rate = 0
+        else:
+            raw_rate = Fraction(number_moved, time_elapsed)
+
+        # time_elapsed += self.tick - intervals[0][0].tick
+
+        self.avg_lookback = 4
+        total = sum([item.raw_rate for item in itertools.islice(reversed(self.rate_log), self.avg_lookback)])
+        total += raw_rate
+        demand_rate = Fraction(total, self.avg_lookback + 1)
+        rlog = RateLogItem(tick=self.tick, raw_rate=raw_rate, demand_rate=demand_rate)
+        self.rate_log.append(rlog)
+
+
+        print(f'Tick: {self.tick}. number_moved: {number_moved}. time_elapsed: {time_elapsed}. rate: {raw_rate} total: {total}, length: {self.avg_lookback + 1}, demand_rate: {demand_rate}')
+
+        # period_older, period_newer = self.get_periods()
+        # if period_older is None or period_newer is None:
+        #     return 0
+
+        # length = period_newer.tick - period_older.tick
+        # num_demanded = period_newer.lt_demanded - period_older.lt_demanded
+        # print(f'Tick: {self.tick}. period length: {length}. number demanded: {num_demanded}')
+        # return Fraction(num_demanded, length)
+
+        return demand_rate
 
     @property
     def storage_complete_rate(self):
@@ -113,21 +159,27 @@ class TestPrefetcher(RateBucket):
 
     def next_action(self):
         next_action = super().next_action()
-        return max(min(self.next_sample, next_action), self.tick)
+        return next_action
+        # return max(min(self.next_sample, next_action), self.tick)
 
     def run(self, *args, **kwargs):
-        if len(self.ledger) > 2:
-            self.log_rates()
-        if self.should_sample():
-            self.sample()
+        self.log_rates()
+        self.tick_data['awaiting_dispatch'] = self.awaiting_dispatch
+        # if len(self.ledger) > 2:
+        #     self.log_rates()
+        # if self.should_sample():
+        #     self.sample()
         super().run(*args, **kwargs)
 
     def log_rates(self):
-        self.tick_data['awaiting_dispatch'] = self.awaiting_dispatch
         self.tick_data['demand_rate'] = float(self.demand_rate)
-        self.tick_data['storage_completed_rate'] = float(self.storage_complete_rate)
-        self.tick_data['awd_rate'] = float(self.awd_rate)
-        self.tick_data['cnc_rate'] = float(self.cnc_rate)
+        self.tick_data['max_iops'] = float(self.pipeline['submitted'].storage_speed)
+        # self.tick_data['storage_completed_rate'] = float(self.storage_complete_rate)
+        # self.tick_data['awd_rate'] = float(self.awd_rate)
+        # self.tick_data['cnc_rate'] = float(self.cnc_rate)
+
+    def adjust(self):
+        pass
 
     def sample(self):
         self.period.length = self.tick - self.period.tick
